fc141222ae80c542a4efb659ee1b70677f5a977c
Yuvraj44 <113596847+Yuvraj44@users.noreply.github.com>
2024-09-04 07:30:10 +0530

    Time: 11 ms (92.09%) | Memory: 16.9 MB (93.16%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,51 +1,95 @@
 class Solution {
 public:
 
-    bool check(int src, vector <int> adj[], vector <int> &vis, vector <int> &pathvis)
+    bool dfs(int src, vector <int> adj[], vector <int> &vis, vector <int> &pathvis)
     {
         vis[src]=1;
         pathvis[src]=1;
 
-        for(auto it : adj[src])
+        for(auto it: adj[src])
         {
             if(vis[it]==0)
             {
-                if(check(it, adj, vis, pathvis)==true)
+                if(dfs(it, adj, vis, pathvis)==true)
                 return true;
             }
-            else if(pathvis[it]==1)
-            return true;
+            else
+            {
+                if(pathvis[it]==1)
+                return true;
+            }
         }
         pathvis[src]=0;
         return false;
     }
 
-    bool isCycle(vector <int> adj[], int n)
+    bool isCycledfs(vector <int> adj[], int n)
     {
-        vector <int> vis(n, 0);
-        vector <int> pathvis(n, 0);
+        vector <int> vis (n, 0);
+        vector <int> pathvis (n, 0);
 
         for(int i=0; i<n; i++)
         {
             if(vis[i]==0)
             {
-                if(check(i, adj, vis, pathvis)==true)
-                return true;
+               if(dfs(i, adj, vis, pathvis)==true)
+               return true;
             }
         }
         return false;
     }
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        
-        for(int i = 0; i < prerequisites.size(); i++) 
-        {
-            int v = prerequisites[i][0], u = prerequisites[i][1];
-            adj[u].push_back(v);
-        }
-        
-        return !(isCycle(adj, numCourses));
+    bool isCyclebfs(vector <int> adj[], int n)
+    {
+        int c=0;
+        vector <int> indegree(n, 0);
+        queue <int> q;
 
+        for(int i=0; i<n; i++)
+        {
+            for(auto it: adj[i])
+            indegree[it]++;
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(indegree[i]==0)
+            q.push(i);
+        }
+
+
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+
+            c++;
+
+            for(auto it: adj[node])
+            {
+                indegree[it]--;
+
+                if(indegree[it]==0)
+                q.push(it);
+            }
+        }
+
+        if(c!=n)
+        return true;
+        else
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector <int> adj[numCourses];
+
+        for(int i=0; i<prerequisites.size(); i++)
+        {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            
+        }
+        bool ans;
+        // ans=!(isCycledfs(adj, numCourses));
+        ans=!(isCyclebfs(adj, numCourses));
+        return ans;
     }
 };
