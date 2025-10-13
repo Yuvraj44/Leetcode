9ce87f6d4bd9827d6ba2a9299b997cc0961fdde3
Yuvraj Singh <113596847+Yuvraj44@users.noreply.github.com>
2025-10-13 10:29:28 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (90.85%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector <int> v;
+        vector <vector <int>> ans;
+        // sort(nums.begin(), nums.end());
+        func(0, nums, v, ans, target);
+        return ans;
+    }
 
-    void func(vector <int> &arr, int ind, int &target, int sum, vector <int> &temp, vector <vector <int>> &ans)
+    void func(int ind, vector <int> &nums,vector <int> &v, vector <vector<int>> &ans, int target)
     {
-        if(ind==arr.size())
-        {
-            if(sum==target)
-            ans.push_back(temp);
+        if(ind==nums.size() || target<0)
+        return;
 
+        if(target==0)
+        {
+            ans.push_back(v);
             return;
         }
 
-        if(sum+arr[ind] <= target)
+       
+
+        if(nums[ind] <= target)
         {
-            temp.push_back(arr[ind]);
-            func(arr, ind, target, sum+arr[ind], temp, ans);
-            temp.pop_back();
+            v.push_back(nums[ind]);
+            func(ind, nums, v, ans, target-nums[ind]);
+            v.pop_back();
         }
-        func(arr, ind+1, target, sum, temp, ans);
-    }
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        vector <vector <int>> ans;
-        vector <int> temp;
+        func(ind+1, nums, v, ans, target);
 
-        func(arr, 0, target, 0, temp, ans);
-
-        return ans;
     }
 };
