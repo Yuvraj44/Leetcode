88398c2e6d4108943e725a6c9c724548161d2c97
Yuvraj Singh <113596847+Yuvraj44@users.noreply.github.com>
2025-10-13 09:56:15 +0530

    Time: 3 ms (31.30%) | Memory: 17 MB (5.83%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector <int> v;
+        vector <vector <int>> ans;
+        func(0, nums, v, ans);
+        return ans;
+    }
 
-    void func(int ind, vector <int> v, vector <int> nums, vector <vector <int>> &ans, int n)
+    void func(int ind, vector <int> nums,   vector <int> v ,vector <vector <int>> &ans)
     {
-        if(ind==n)
+        if(ind==nums.size())
         {
             ans.push_back(v);
-            v.clear();
             return;
         }
 
         v.push_back(nums[ind]);
-        func(ind+1, v, nums, ans, n);
+        func(ind+1, nums, v, ans);
         v.pop_back();
-        func(ind+1, v, nums, ans, n);
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector <int> v;
-        vector <vector <int>> ans;
-        func(0, v, nums, ans, nums.size());
-        return ans;
+        func(ind+1, nums, v, ans);
     }
 };
