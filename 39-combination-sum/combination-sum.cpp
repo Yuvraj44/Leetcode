ba34a14b59b4f14407dc87417d24fabb675cf915
Yuvraj Singh <113596847+Yuvraj44@users.noreply.github.com>
2025-10-26 20:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (72.96%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -3,24 +3,20 @@ public:
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         vector <int> v;
         vector <vector <int>> ans;
-        // sort(nums.begin(), nums.end());
         func(0, nums, v, ans, target);
         return ans;
     }
 
     void func(int ind, vector <int> &nums,vector <int> &v, vector <vector<int>> &ans, int target)
     {
-        if(ind==nums.size() || target<0)
-        return;
-
-        if(target==0)
+        if(ind==nums.size())
         {
-            ans.push_back(v);
-            return;
-        }
+           if(target==0)
+           ans.push_back(v);
 
-       
-
+           return;        
+        } 
+        
         if(nums[ind] <= target)
         {
             v.push_back(nums[ind]);
